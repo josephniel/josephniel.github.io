29a3aae892bc80e027b583aab9cb51bde62c1e56
Joseph Niel Tuazon <josephnieltuazon@yahoo.com>
2016-08-01 00:53:53 +0800

    Deleted everything

--- README.md
+++
@@ -1,3 +0,0 @@
-# josephniel.github.io
-
-This repository represents Joseph Tuazon's online portfolio (which is also available [here](http://agila.upm.edu.ph/~jntuazon)).
--- public/js/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(){document.addEventListener("keypress",function(e){console.log("key pressed");var o=e.keyCode,n=document.body;40==o&&(n.style.marginTop-=n.scrollTop,console.log("down"))})}();
-//# sourceMappingURL=app.js.map
